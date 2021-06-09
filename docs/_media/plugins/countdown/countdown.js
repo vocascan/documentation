9796ee69d2a1b33b1affb6ae68d34c1e8e72ed17
Zicheng Wang <zicheng.wang@interweber.de>
2021-06-09 20:24:01 +0200

    chore: countdown design

--- docs/_media/plugins/countdown/countdown.js
+++ docs/_media/plugins/countdown/countdown.js
@@ -5,10 +5,10 @@ function plugin(hook) {
     countdowns.forEach(function (countdown) {
       countdown.innerHTML = `
           <ul>
-            <li><span class="days"></span>days</li>
-            <li><span class="hours"></span>Hours</li>
-            <li><span class="minutes"></span>Minutes</li>
-            <li><span class="seconds"></span>Seconds</li>
+            <li><span class="days"></span><span class="unit">days</span></li>
+            <li><span class="hours"></span><span class="unit">Hours</span></li>
+            <li><span class="minutes"></span><span class="unit">Minutes</span></li>
+            <li><span class="seconds"></span><span class="unit">Seconds</span></li>
           </ul>`;
 
       const second = 1000,
@@ -19,14 +19,20 @@ function plugin(hook) {
       const date = new Date(countdown.dataset.endDate).getTime();
       let id = null;
 
+      function leadingZero(num) {
+        return num < 10 ? "0" + num : num;
+      }
+
       function step() {
         const now = new Date().getTime();
         const distance = date - now;
 
-        countdown.getElementsByClassName("days")[0].innerText = Math.floor(distance / day);
-        countdown.getElementsByClassName("hours")[0].innerText = Math.floor((distance % day) / hour);
-        countdown.getElementsByClassName("minutes")[0].innerText = Math.floor((distance % hour) / minute);
-        countdown.getElementsByClassName("seconds")[0].innerText = Math.floor((distance % minute) / second);
+        countdown.getElementsByClassName("days")[0].innerText = leadingZero(Math.floor(distance / day));
+        countdown.getElementsByClassName("hours")[0].innerText = leadingZero(Math.floor((distance % day) / hour));
+        countdown.getElementsByClassName("minutes")[0].innerText = leadingZero(Math.floor((distance % hour) / minute));
+        countdown.getElementsByClassName("seconds")[0].innerText = leadingZero(
+          Math.floor((distance % minute) / second)
+        );
 
         if (distance < 0) {
           countdown.innerHTML = countdown.dataset.finishLabel;
