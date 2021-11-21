da027fef3dc5f322c91effa9b832ced7045b98f6
luwol03 <luwol03@gmail.com>
2021-11-21 16:51:11 +0100

    fixed auto link plugin

--- docs/_media/plugins/autoLink/autoLink.js
+++ docs/_media/plugins/autoLink/autoLink.js
@@ -2,7 +2,13 @@ function plugin(hook, vm) {
   hook.beforeEach(function (content) {
     return content.replace(/(\S+\/\S+)?#(\d+)/gm, (fullDescription, repo, number) => {
       if (!repo) {
-        repo = "vocascan/" + vm.route.file.split("/")[0];
+        const match = vm.route.file.match(/https:\/\/.*?\/(\S+?\/\S+?)\//);
+
+        if (match) {
+          repo = match[1];
+        } else {
+          repo = "vocascan/" + vm.route.file.split("/")[0];
+        }
       }
 
       return "[" + fullDescription + "](https://github.com/" + repo + "/pull/" + number + ")";
